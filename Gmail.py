5823259607979161bcb4d7f24c7231992cfdc738
hamed <shiani@outlook.com>
2022-09-12 17:48:58 +0430

    Add documentation to files

--- Gmail.py
+++ Gmail.py
@@ -1,6 +1,6 @@
 
 #!/usr/bin/env python
-
+from .Message import Message
 from base64 import urlsafe_b64decode
 import email
 import os
@@ -12,6 +12,14 @@ from googleapiclient.discovery import build
 
 
 class SendMail:
+    """Send Mail Class.
+
+    This class is used to send email.
+
+    Args:
+        service : An instance of your email service (google, ...).
+
+    """
 
     def __init__(self, service) -> None:
         self.service = service    
@@ -21,29 +29,30 @@ class SendMail:
 
 
 class SearchMail:
+    """Search Mail Class.
 
+    This class is used to search a query in INBOX or SENTBOX of service.
+    This also downloads the attachments of that mails into seprated folder in the folder that code executed.
+
+    Args:
+        service : An instance of your email service (google, ...).
+
+    Returns:
+        a list of founded mails in SENTBOX or INBOX, 
+            include LabelIds, Subject, From, To, Date, Attachments and Body of the mail.
+    
+    """
     def __init__(self, service) -> None:
         self.service = service
 
-    @staticmethod
-    def get_size_format(b, factor=1024, suffix="B"):
-        """
-        Scale bytes to its proper byte format
-        e.g:
-            1253656 => '1.20MB'
-            1253656678 => '1.17GB'
-        """
-        for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
-            if b < factor:
-                return f"{b:.2f}{unit}{suffix}"
-            b /= factor
-        return f"{b:.2f}Y{suffix}"
     
-    def search_by_query(self, query):
+    def search_by_query(self, query: str) -> list:
+        # grab the message instances of searched query 
         result = self.service.users().messages().list(userId='me', q=query).execute()
-        # print(result)
         mail_list = list()
+        # if any message found the result include 'messages' key
         if result.get('messages', None):
+
             for item in result['messages']:
                 # grab the message instance
                 message = self.service.users().messages().get(userId='me', id=item['id'],format='raw').execute()
@@ -58,6 +67,7 @@ class SearchMail:
                 # return the encoded text
                 final_content = parts[0]
                 
+                # create mail dictionary
                 mail = dict()
                 mail['LabelIds'] = message['labelIds'][0]
                 mail['Subject'] = mime_msg['Subject']
@@ -65,6 +75,8 @@ class SearchMail:
                 mail['To'] = mime_msg['To']
                 mail['Date'] = mime_msg['Date']
                 mail['Attachments'] = []
+
+                # request the mail again with full format to grab the payloads of mail
                 msg = self.service.users().messages().get(userId='me', id=item['id'], format='full').execute()
                 payload = msg['payload']
                 # headers = payload.get("headers")
@@ -104,6 +116,7 @@ class SearchMail:
                                             .attachments().get(id=attachment_id, userId='me', messageId=message['id']).execute()
                                 data = attachment.get("data")
                                 filepath = os.path.join(file_path, filename)
+                                # write data in folder
                                 if data:
                                     with open(filepath, "wb") as f:
                                         f.write(urlsafe_b64decode(data))
@@ -121,36 +134,52 @@ class SearchMail:
         return mail_list
 
 class Gmail:
-    def __init__(self) -> None:
+
+    """
+    Gmail class for Simplification gmail send mail and search query.
+
+    Args:
+        cred_file_path: path of your cred.json file
+
+    Raises :
+        ConnectionError if gmail can't authenticate 
+                    authentication needs a cred.json path.
+
+    """
+    def __init__(self, cred_file_path) -> None:
         self._cred = None
         self._scopes = ['https://mail.google.com/']
         self.__authenticate__()
+        self.cred_file = cred_file_path
 
 
     def __authenticate__(self) -> None:
-        # if user has credentials
-        if os.path.exists("token.pickle"):
-            with open("token.pickle", "rb") as token:
-                self._cred = pickle.load(token)
+        try:
+            # if user has credentials
+            if os.path.exists("token.pickle"):
+                with open("token.pickle", "rb") as token:
+                    self._cred = pickle.load(token)
 
-        # if there are no (valid) credentials availablle, let the user log in.
-        if not self._cred or not self._cred.valid:
-            if self._cred and self._cred.expired and self._cred.refresh_token:
-                self._cred.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file('cred.json', self._scopes)
-                self._cred = flow.run_local_server(port=0)
-            # save the credentials for the next run
-            with open("token.pickle", "wb") as token:
-                pickle.dump(self._cred, token)
-        self.service = build('gmail', 'v1', credentials=self._cred)
+            # if there are no (valid) credentials availablle, let the user log in.
+            if not self._cred or not self._cred.valid:
+                if self._cred and self._cred.expired and self._cred.refresh_token:
+                    self._cred.refresh(Request())
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(self.cred_file, self._scopes)
+                    self._cred = flow.run_local_server(port=0)
+                # save the credentials for the next run
+                with open("token.pickle", "wb") as token:
+                    pickle.dump(self._cred, token)
+            self.service = build('gmail', 'v1', credentials=self._cred)
+        except:
+            raise ConnectionError("Authentication failed")
 
 
-    def send_mail(self, message):
+    def send_mail(self, message: Message):
 
         return SendMail(self.service).send_mail(message)
     
-    def search_mail(self, query: str):
+    def search_mail(self, query: str) -> list:
 
         return SearchMail(self.service).search_by_query(query)
 
--- Message.py
+++ Message.py
@@ -9,52 +9,54 @@ from base64 import urlsafe_b64encode
 import os
 
 class Message:
+    """Message Class creates an message object to send with mail service.
+    Args:
+        From : a name that you want to show in email sender.
+        To: destination of your message.
+        subject: subject of your message.
+        body: body text of your message.
+        attachments: list of atachments paths
 
-    def __init__(self, destination, subject, body, attachments=[]) -> None:
-        self.destination = destination
+    """
+    def __init__(self,From: str, To: str, subject: str, body: str, attachments: list =[]) -> None:
+        self.To = To
+        self.From = From
         self.subject = subject
         self.body = body
         self.attachments = attachments
+        
 
     @staticmethod
-    def add_attachment(message, filename):
+    def add_attachment(message, filename) ->None:
         content_type, encoding = guess_mime_type(filename)
         if content_type is None or encoding is not None:
             content_type = 'application/octet-stream'
         main_type, sub_type = content_type.split('/', 1)
-        if main_type == 'text':
-            fp = open(filename, 'rb')
-            msg = MIMEText(fp.read().decode(), _subtype=sub_type)
+
+        with open(filename, 'rb') as fp:
+            if main_type == 'text':
+                msg = MIMEText(fp.read().decode(), _subtype=sub_type)
+            elif main_type == 'image':
+                msg = MIMEImage(fp.read(), _subtype=sub_type)
+            elif main_type == 'audio':
+                msg = MIMEAudio(fp.read(), _subtype=sub_type)
+            else:
+                msg = MIMEBase(main_type, sub_type)
+                msg.set_payload(fp.read())
             fp.close()
-        elif main_type == 'image':
-            fp = open(filename, 'rb')
-            msg = MIMEImage(fp.read(), _subtype=sub_type)
-            fp.close()
-        elif main_type == 'audio':
-            fp = open(filename, 'rb')
-            msg = MIMEAudio(fp.read(), _subtype=sub_type)
-            fp.close()
-        else:
-            fp = open(filename, 'rb')
-            msg = MIMEBase(main_type, sub_type)
-            msg.set_payload(fp.read())
-            fp.close()
+
         filename = os.path.basename(filename)
+        # add headers
         msg.add_header('Content-Disposition', 'attachment', filename=filename)
         message.attach(msg)
 
-    def build_message(self):
-        if not self.attachments: # no attachments given
-            message = MIMEText(self.body)
-            message['to'] = self.destination
-            message['from'] = "test26391@gmail.com"
-            message['subject'] = self.subject
-        else:
-            message = MIMEMultipart()
-            message['to'] = self.destination
-            message['from'] = "test26391@gmail.com"
-            message['subject'] = self.subject
-            message.attach(MIMEText(self.body))
+    def build_message(self) -> dict:
+        message = MIMEMultipart()
+        message['to'] = self.To
+        message['from'] = self.From
+        message['subject'] = self.subject
+        message.attach(MIMEText(self.body))
+        if not self.attachments: 
             for filename in self.attachments:
                 self.add_attachment(message, filename)
 
